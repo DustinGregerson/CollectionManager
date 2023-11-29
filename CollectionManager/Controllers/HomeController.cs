7644a8d833c8db96cd86278a64220984049feebe
DustinGregerson <dustin.gregerson@southeasttech.edu>
2023-11-29 10:35:29 -0600

    MVC template blank slate.

--- CollectionManager/Controllers/HomeController.cs
+++ CollectionManager/Controllers/HomeController.cs
@@ -1,6 +1,10 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
 
+//Author: Dustin Gregerson
+//Date: 11/29/2023
+//Description: main routing controller
+
 namespace CollectionManager.Controllers
 {
     public class HomeController : Controller
